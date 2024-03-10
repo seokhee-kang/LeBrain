0f00f0b845e638ca248ccf4bcce9ca696954a27a
Aymeric Degroote <aymeric.degroote@gmail.com>
2024-03-09 23:23:49 -0500

    transfering files to lab computer

---
+++ physiolabxr-scripts/LSLExampleOutlet.py
@@ -0,0 +1,44 @@
+"""Example program to demonstrate how to send a multi-channel time series to
+LSL."""
+import random
+import sys
+import string
+import numpy as np
+import time
+from random import random as rand
+
+from pylsl import StreamInfo, StreamOutlet, local_clock
+
+
+def main(argv):
+    srate = 128
+    name = 'fake_obci_emg1'
+    print('Stream name is ' + name)
+    type = 'EEG'
+    n_channels = 8
+    info = StreamInfo(name, type, n_channels, srate, 'float32', 'someuuid1234')
+
+    # next make an outlet
+    outlet = StreamOutlet(info)
+
+    print("now sending data...")
+    start_time = local_clock()
+    sent_samples = 0
+    while True:
+        elapsed_time = local_clock() - start_time
+        required_samples = int(srate * elapsed_time) - sent_samples
+        
+        for sample_ix in range(required_samples):
+            # make a new random n_channels sample; this is converted into a
+            # pylsl.vectorf (the data type that is expected by push_sample)
+           
+            mysample = [rand()*1 for _ in range(n_channels)]
+            # now send it
+            outlet.push_sample(mysample)
+        sent_samples += required_samples
+        # now send it and wait for a bit before trying again.
+        time.sleep(0.01)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
---
+++ physiolabxr-scripts/binarize_emg.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+from physiolabxr.scripting.RenaScript import RenaScript
+
+
+class emg_script(RenaScript):
+    def __init__(self, *args, **kwargs):
+        """
+        Please do not edit this function
+        """
+        super().__init__(*args, **kwargs)
+
+    # Start will be called once when the run button is hit.
+    def init(self):
+        print('init function is called')
+        pass
+
+    # loop is called <Run Frequency> times per second
+    # 250 Hz?
+    def loop(self):
+        
+        data = self.inputs.get_data('fake_obci_emg1')
+        timestamps = self.inputs.get_timestamps('fake_obci_emg1')
+
+        # number of channels to keep
+        nb_channels = self.params['nb_channels']
+
+        # keep the first nb_channels channels 
+        data = data[:nb_channels]
+
+        # for each channel, binarize the value: 1 if above the given threshold
+        for i in range(nb_channels):
+            # there is a threshold for each channel
+            data[i] = [1 * (x >= self.params[f'threshold_{i}']) for x in data[i]]
+
+        print('data', data[0][:20])
+
+        # output data is boolean
+        self.set_output(stream_name="emg_bin", data=data, timestamp=timestamps)
+
+        self.inputs.clear_buffer_data()
+
+    # cleanup is called when the stop button is hit
+    def cleanup(self):
+        print('Cleanup function is called')
